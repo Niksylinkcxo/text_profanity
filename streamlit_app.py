ca2fcba517b960eacdfd0f981588696b28ffc178
LinkCxO <167839362+Niksylinkcxo@users.noreply.github.com>
2024-09-24 11:17:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,54 +1,39 @@
-import streamlit as st
-import spacy
-import numpy as np
-import pandas as pd
-from sklearn.metrics.pairwise import cosine_similarity
-from transformers import pipeline
 import re
-import emoji
 import nltk
+import pandas as pd
+import emoji
+import streamlit as st
+from transformers import pipeline
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 from nltk.corpus import stopwords
-from indic_transliteration import sanscript
-from indic_transliteration.sanscript import transliterate
-from googletrans import Translator
+from azure.storage.blob import BlobServiceClient
+import io
 
-# Initialize NLP and other resources
-nlp = spacy.load('en_core_web_lg')
+# Download stopwords list if not already present
+nltk.download('stopwords')
+nltk.download('punkt')
+
+# Initialize stopwords set
 stop_words = set(stopwords.words('english')).union(ENGLISH_STOP_WORDS)
-classifier = pipeline('text-classification', model="parsawar/profanity_model_3.1")
 
-# Load the vector data
-vector_csv_path = '.\vector_data.csv'
-
-def load_csv_with_fallback(csv_path):
-    try:
-        df = pd.read_csv(csv_path, encoding='utf-8')
-    except UnicodeDecodeError:
-        df = pd.read_csv(csv_path, encoding='latin-1')
-    return df
-
-vector_df = load_csv_with_fallback(vector_csv_path)
-
-def load_words(file_path):
-    with open(file_path, 'r') as file:
-        return set(line.strip().lower() for line in file)
-
-forbidden_words_path = '.\abusive_words.txt'
-forbidden_words = load_words(forbidden_words_path)
-
+# Function to remove standalone special characters
 def remove_special_characters(text):
     pattern = r'(?<!\w)[^\w\s](?!\w)'
     return re.sub(pattern, '', text)
 
+# Function to convert text to lowercase
 def convert_to_lowercase(text):
     return text.lower()
 
+# Function to convert emojis to textual representation
 def convert_emojis(text):
     return emoji.demojize(text)
 
+# Function to remove stopwords
 def remove_stopwords(text):
     return ' '.join([word for word in text.split() if word not in stop_words])
 
+# Function to preprocess text
 def preprocess_text(text):
     text = remove_special_characters(text)
     text = convert_to_lowercase(text)
@@ -56,106 +41,158 @@ def preprocess_text(text):
     text = remove_stopwords(text)
     return text
 
-def transliterate_and_translate(text):
-    transliterated_text = transliterate(text, sanscript.DEVANAGARI, sanscript.ITRANS)
-    translator = Translator()
-    translation = translator.translate(transliterated_text, src='hi', dest='en')
-    return transliterated_text, translation.text
+# Sample list of profane words
+PROFANE_WORDS = [
+    '2 girls 1 cup', 'anal', 'anus', 'areole', 'arian', 'arrse', 'arse', 'arsehole', 
+    'aryan', 'ass', 'ass-fucker', 'assbang', 'assbanged', 'asses', 'assfuck', 'assfucker', 
+    'assfukka', 'asshole', 'assmunch', 'asswhole', 'auto erotic', 'autoerotic', 'ballsack', 
+    'bastard', 'bdsm', 'beastial', 'beastiality', 'bellend', 'bestial', 'bestiality', 'bimbo', 
+    'bimbos', 'bitch', 'bitches', 'bitchin', 'bitching', 'blow job', 'blowjob', 'blowjobs', 
+    'blue waffle', 'bondage', 'boner', 'boob', 'boobs', 'booobs', 'boooobs', 'booooobs', 
+    'booooooobs', 'booty call','assshole', 'breasts', 'brown shower', 'brown showers', 'buceta', 
+    'bukake', 'bukkake', 'bull shit', 'bullshit', 'busty', 'butthole', 'carpet muncher', 
+    'cawk', 'chink', 'cipa', 'clit', 'clitoris', 'clits', 'cnut', 'cock', 'cockface', 
+    'cockhead', 'cockmunch', 'cockmuncher', 'cocks', 'cocksuck', 'cocksucked', 'cocksucker', 
+    'cocksucking', 'cocksucks', 'cokmuncher', 'coon', 'cow-girl', 'cow-girls', 'cowgirl', 
+    'cowgirls', 'crap', 'crotch', 'cum', 'cuming', 'cummer', 'cumming', 'cums', 'cumshot', 
+    'cunilingus', 'cunillingus', 'cunnilingus', 'cunt', 'cuntlicker', 'cuntlicking', 'cunts', 
+    'damn', 'deep throat', 'deepthroat', 'dick', 'dickhead', 'dildo', 'dildos', 'dink', 
+    'dinks', 'dlck', 'dog style', 'dog-fucker', 'doggie style', 'doggie-style', 'doggiestyle', 
+    'doggin', 'dogging', 'doggy style', 'doggy-style', 'doggystyle', 'dong', 'donkeyribber', 
+    'doofus', 'doosh', 'dopey', 'douch3', 'douche', 'douchebag', 'douchebags', 'douchey', 
+    'drunk', 'duche', 'dumass', 'dumbass', 'dumbasses', 'dyke', 'dykes', 'eatadick', 
+    'eathairpie', 'ejaculate', 'ejaculated', 'ejaculates', 'ejaculating', 'ejaculatings', 
+    'ejaculation', 'ejakulate', 'enlargement', 'erect', 'erection', 'erotic', 'erotism', 
+    'essohbee', 'extacy', 'extasy', 'f_u_c_k', 'f-u-c-k', 'f.u.c.k', 'f4nny', 'facial', 
+    'fack', 'fag', 'fagg', 'fagged', 'fagging', 'faggit', 'faggitt', 'faggot', 'faggs', 
+    'fagot', 'fagots', 'fags', 'faig', 'faigt', 'fanny', 'fannybandit', 'fannyflaps', 
+    'fannyfucker', 'fanyy', 'fart', 'fartknocker', 'fat', 'fatass', 'fcuk', 'fcuker', 'motherfucker',
+    'fcuking', 'feck', 'fecker', 'felch', 'felcher', 'felching', 'fellate', 'fellatio', 
+    'feltch', 'feltcher', 'femdom', 'fingerfuck', 'fingerfucked', 'fingerfucker', 
+    'fingerfuckers', 'fingerfucking', 'fingerfucks', 'fingering', 'fisted', 'fistfuck', 
+    'fistfucked', 'fistfucker', 'fistfuckers', 'fistfucking', 'fistfuckings', 'fistfucks', 
+    'fisting', 'fisty', 'flange', 'flogthelog', 'floozy', 'foad', 'fondle', 'foobar', 
+    'fook', 'fooker', 'foot job', 'footjob', 'foreskin', 'freex', 'frigg', 'frigga', 
+    'fubar', 'fuck', 'fuck-ass', 'fuck-bitch', 'fuck-tard', 'fucka', 'fuckass', 'fucked', 
+    'fucker', 'fuckers', 'fuckface', 'fuckhead', 'fuckheads', 'fuckhole', 'fuckin', 
+    'fucking', 'fuckings', 'fuckingshitmotherfucker', 'fuckme', 'fuckmeat', 'fucknugget', 
+    'fucknut', 'fuckoff', 'fuckpuppet', 'fucks', 'fucktard', 'fucktoy', 'fucktrophy', 
+    'fuckup', 'fuckwad', 'fuckwhit', 'fuckwit', 'fuckyomama', 'fudgepacker', 'fuk', 
+    'fuker', 'fukker', 'fukkin', 'fukking', 'fuks', 'fukwhit', 'fukwit', 'futanari', 
+    'futanary', 'fux', 'fux0r', 'fvck', 'fxck', 'g-spot', 'gae', 'gai', 'gang bang', 
+    'gang-bang', 'gangbang', 'gangbanged', 'gangbangs', 'ganja', 'gassyass', 'gay', 
+    'gaylord', 'gays', 'gaysex', 'gey', 'gfy', 'ghay', 'ghey', 'gigolo', 'glans', 
+    'goatse', 'god', 'god-dam', 'god-damned', 'godamn', 'godamnit', 'goddam', 'goddammit', 
+    'goddamn', 'goddamned', 'gokkun', 'golden shower', 'goldenshower', 'gonad', 'gonads', 
+    'gook', 'gooks', 'gringo', 'gspot', 'gtfo', 'guido', 'h0m0', 'h0mo', 'hamflap', 
+    'hand job', 'handjob', 'hardcoresex', 'hardon', 'he11', 'hebe', 'heeb', 'hell', 
+    'hemp', 'hentai', 'heroin', 'herp', 'herpes', 'herpy', 'heshe', 'hitler', 'hiv', 
+    'hoar', 'hoare', 'hobag', 'hoer', 'hom0', 'homey', 'homo', 'homoerotic', 'homoey', 
+    'honky', 'hooch', 'hookah', 'hooker', 'hoor', 'hootch', 'hooter', 'hooters', 
+    'hore', 'horniest', 'horny', 'hotsex', 'howtokill', 'howtomurdep', 'hump', 'humped', 
+    'humping', 'hussy', 'hymen', 'inbred', 'incest', 'injun', 'j3rk0ff', 'jack off', 
+    'jack-off', 'jackass', 'jackhole', 'jackoff', 'jap', 'japs', 'jerk', 'jerk off', 
+    'jerk-off', 'jerk0ff', 'jerked', 'jerkoff', 'jism', 'jiz', 'jizm', 'jizz', 'jizzed', 
+    'junkie', 'junky', 'kawk', 'kike', 'kikes', 'kill', 'kinbaku', 'kinky', 'kinkyJesus', 
+      'knob', 'kock', 'kondom', 'konk', 'kunt', 'kuntlicker', 'kuntlicking', 'kuntz', 
+    'kyke', 'l3tters', 'l33t', 'l3tters', 'leather', 'lesbian', 'lezzie', 'lube', 
+    'masturbate', 'masturbation', 'mofo', 'mofos', 'mutha', 'muthafucka', 'muthafuckas', 
+    'muthafuckin', 'muthafucking', 'n1gga', 'n1gger', 'nazi', 'nigga', 'nigger', 
+    'niggers', 'nutsack', 'orally', 'p0rn', 'p0rn0', 'p3nis', 'p4k', 'paki', 'pano', 
+    'panties', 'pecker', 'peeing', 'piss', 'pissed', 'pissin', 'pissin', 'pissing', 
+    'playboy', 'poof', 'poon', 'poop', 'porn', 'porn0', 'pornography', 'pr0n', 
+    'puss', 'pussy', 'qu33r', 'queer', 'rape', 'rectum', 'retard', 'rimjob', 'roastbeef', 
+    'rubandtug', 's3x', 's3x0', 's3xual', 's3xuality', 's3xy', 'sadist', 'shemale', 
+    'sh1t', 'sh1tcock', 'sh1tdick', 'sh1tface', 'sh1tfaced', 'sh1thead', 'sh1thole', 
+    'sh1ts', 'sh1tster', 'sh1tty', 'sh1z', 'shlong', 'shyte', 'sickfuck', 'slut', 
+    'sluts', 'smut', 'snatch', 'sodomy', 'spacelube', 'spunk', 'stfu', 'strapon', 
+    'suck', 'sucker', 'sucking', 't1tt1e5', 't1tties', 'teabagger', 'testicle', 
+    'threesome', 'titt', 'tits', 'titwank', 'tosser', 'tranny', 'trannies', 'turd', 
+    'twat', 'vag', 'vagina', 'wank', 'wanker', 'wetback', 'wh0r3', 'whore', 'w0r3', 
+    'wtf', 'yank', 'yiffy', 'z0mg', 'zoophile', 'zoophilia'
+]
 
-def contains_forbidden_word(input_text, forbidden_words):
-    words = input_text.split()
-    return any(word.lower() in forbidden_words for word in words)
+# Initialize Blob Service Client
+connection_string = 'DefaultEndpointsProtocol=https;AccountName=stagebucket;AccountKey=your_account_key;EndpointSuffix=core.windows.net'
+container_name = 'linkcxo'
+blob_name = 'user_inputs.csv'
 
-def process_user_input(user_input, vector_df=vector_df, nlp=nlp, forbidden_words=forbidden_words):
-    transliterated_text = None
-    translated_text = None
+blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+container_client = blob_service_client.get_container_client(container_name)
 
-    def save_entries(entries):
-        new_entries_df = pd.DataFrame(entries)
-        vector_df_updated = pd.concat([vector_df, new_entries_df], ignore_index=True)
-        vector_df_updated.to_csv(vector_csv_path, index=False)
-        return vector_df_updated
+def append_to_csv(text, label):
+    # Create a DataFrame with the new data
+    df = pd.DataFrame({'text': [text], 'label': [label]})
 
-    if re.search('[\u0900-\u097F]', user_input):
-        transliterated_text, translated_text = transliterate_and_translate(user_input)
-        translated_for_processing = translated_text
+    # Convert DataFrame to CSV
+    csv_buffer = io.StringIO()
+    df.to_csv(csv_buffer, index=False, header=False)
 
-        original_vector = nlp(user_input).vector
-        entries = [{
-            "cleaned_text": user_input,
-            "label": 1,
-            "vector": ','.join(map(str, original_vector))
-        }]
+    # Upload the CSV to Azure Blob Storage
+    blob_client = container_client.get_blob_client(blob_name)
+    
+    try:
+        # Download the existing CSV
+        existing_blob = blob_client.download_blob()
+        existing_data = existing_blob.readall().decode('utf-8')
+    except:
+        # If blob doesn't exist, create it with new data
+        existing_data = ''
+    
+    # Append new data
+    updated_csv = existing_data + csv_buffer.getvalue()
 
-        vector_df = save_entries(entries)
-    else:
-        translated_for_processing = user_input
+    # Upload the updated CSV
+    blob_client.upload_blob(updated_csv, overwrite=True)
 
-    if contains_forbidden_word(translated_for_processing, forbidden_words):
-        return None, None, None, None
+# Function to detect profanity using regex and return the profane words found
+def detect_profanity_with_regex(text, profane_words=PROFANE_WORDS):
+    pattern = re.compile(r'\b(?:' + '|'.join(map(re.escape, profane_words)) + r')\b', re.IGNORECASE)
+    profane_words_found = pattern.findall(text)
+    
+    if profane_words_found:
+        return True, profane_words_found  # Return True and the list of profane words found
+    return False, []
 
-    cleaned_user_input = preprocess_text(translated_for_processing)
-    user_doc = nlp(cleaned_user_input)
-    user_vector = user_doc.vector
+# Load the Hugging Face model for classification
+classifier = pipeline('text-classification', model="parsawar/profanity_model_3.1")
 
-    vectors = vector_df['vector'].apply(lambda x: np.array([float(i) for i in x.split(',')]) if isinstance(x, str) else np.zeros(len(user_vector))).values
-    vectors = np.stack(vectors)
+# Streamlit app layout
+st.title("Profanity Detection App")
 
-    similarities = cosine_similarity([user_vector], vectors)
+# Input text
+user_input = st.text_area("Enter text to check for profanity")
 
-    most_similar_index = similarities[0].argmax()
-    most_similar_entry = vector_df.iloc[most_similar_index]
-    similarity_score = similarities[0][most_similar_index]
+# Preprocess and detect profanity
+if st.button("Check for Profanity"):
+    if user_input:
+        # Clean and preprocess the input text
+        cleaned_text = preprocess_text(user_input.strip())
 
-    threshold = 0.8
+        # Detect profanity using regex
+        profane_detected, profane_words_found = detect_profanity_with_regex(cleaned_text)
 
-    if (similarity_score >= threshold):
-        assigned_label = most_similar_entry['label']
-        hf_label_score = None
-        assigned_based_on_similarity = True
-    else:
-        result = classifier(cleaned_user_input)
-        classified_label = result[0]['label']
-        hf_label_score = result[0]['score']
-
-        assigned_label = classified_label
-        assigned_based_on_similarity = False
-
-    new_vector_entry = {
-        "cleaned_text": translated_for_processing,
-        "label": assigned_label,
-        "vector": ','.join(map(str, user_vector))
-    }
-
-    new_vector_entry_df = pd.DataFrame([new_vector_entry])
-    vector_df = pd.concat([vector_df, new_vector_entry_df], ignore_index=True)
-
-    vector_df.to_csv(vector_csv_path, index=False)
-
-    return similarity_score, assigned_label, hf_label_score, assigned_based_on_similarity
-
-def main():
-    st.title("Text Profanity Detection")
-
-    text_input = st.text_area("Enter text:", "")
-
-    if st.button("Submit"):
-        if not text_input:
-            st.warning("Please enter some text.")
+        if profane_detected:
+            profane_words_string = ', '.join(profane_words_found)
+            response = {
+                'label': "1",  # Profanity detected
+                'profane_words': profane_words_string
+            }
+            st.write("Profanity detected:", profane_words_string)
         else:
-            similarity_score, assigned_label_similarity, hf_label_score, assigned_based_on_similarity = process_user_input(text_input, vector_df, nlp, forbidden_words)
-            
-            if similarity_score is None:
-                st.error("Your text contains restricted words. Please remove them and try again.")
-            else:
-                if assigned_based_on_similarity:
-                    st.success(f"Label: {assigned_label_similarity}")
-                    st.info(f"Score: {similarity_score}")
-                    st.info(f"Determined using: vector similarity")
-                else:
-                    st.success(f"Label: {assigned_label_similarity}")
-                    st.info(f"Score: {hf_label_score}")
-                    st.info(f"Determined using: Hugging Face model")
+            # If no profane words are found, classify the text using Hugging Face model
+            result = classifier(cleaned_text)
+            classified_label = result[0]['label']
+            hf_label_score = result[0]['score']
+            response = {
+                'label': classified_label,
+                'score': hf_label_score,
+                'profane_words': None
+            }
+            st.write(f"Classification result: {classified_label} (Score: {hf_label_score})")
 
-if __name__ == '__main__':
-    main()
+        # Append the result to the CSV
+        append_to_csv(user_input, response['label'])
+    else:
+        st.error("Please enter some text.")
+
